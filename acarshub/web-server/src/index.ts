06b1582663a4407c068e75fcc8b4895b75bfb1d8
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-09-30 13:17:50 -0600

    CSS build

--- acarshub/web-server/src/index.ts
+++ acarshub/web-server/src/index.ts
@@ -10,6 +10,7 @@ const app = express();
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, "../../web-client/dist")));
+app.use(express.static(path.join(__dirname, "../../web-css/dist")));
 
 app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "../../web-client/dist/index.html"));
