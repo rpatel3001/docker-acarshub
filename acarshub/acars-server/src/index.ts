48006edff28aefbc4b388aca9ded01a601b02b47
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-09-30 17:45:38 -0600

    basic web socket test

--- acarshub/acars-server/src/index.ts
+++ acarshub/acars-server/src/index.ts
@@ -1,4 +1,5 @@
 const commandLineArgs = require("command-line-args");
+import { Server, Socket } from "socket.io";
 
 const commandOptions = [{ name: "db-save-all", type: Boolean, default: false }];
 
@@ -12,4 +13,29 @@ commandOptions.forEach((option) => {
     options[option.name] = option.default;
   }
 });
+
 console.log(options);
+
+// Socket setup
+const io = new Server(
+  3000
+  // {
+  // cors: {
+  //   origin: false,
+  // },
+  //
+  //}
+);
+
+io.on("connection", (socket: Socket) => {
+  console.log("Made socket connection");
+
+  socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
+
+  // receive a message from the client
+  socket.on("hello from client", (...args) => {
+    console.log("ping from client");
+  });
+});
+
+console.log("Server started");
