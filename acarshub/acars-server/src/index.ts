c894e1e930eb303a51b60a16742c2b0e0ddbf053
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-09-30 18:23:14 -0600

    Logger

--- acarshub/acars-server/src/index.ts
+++ acarshub/acars-server/src/index.ts
@@ -1,20 +1,56 @@
 const commandLineArgs = require("command-line-args");
+import { createLogger, format, transports, level } from "winston";
 import { Server, Socket } from "socket.io";
+const { combine, timestamp, label, printf } = format;
 
-const commandOptions = [{ name: "db-save-all", type: Boolean, default: false }];
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${level.toUpperCase()}][${label.toUpperCase()}]: ${message}`;
+});
+
+const commandOptions = [
+  { name: "db-save-all", type: Boolean, default: false },
+  { name: "log-level", type: Number, default: 3 },
+];
 
 const options = commandLineArgs(commandOptions);
+let log_level = "info";
+
+if (
+  options["log-level"] &&
+  options["log-level"] >= 3 &&
+  options["log-level"] <= 5
+) {
+  switch (options["log-level"]) {
+    case 3:
+      log_level = "info";
+      break;
+    case 4:
+      log_level = "verbose";
+      break;
+    case 5:
+      log_level = "debug";
+      break;
+  }
+}
+
+const logger = createLogger({
+  level: log_level,
+  transports: [new transports.Console()],
+  format: combine(label({ label: "ACARS Hub Server" }), timestamp(), myFormat),
+});
 
 commandOptions.forEach((option) => {
   if (!options.hasOwnProperty(option.name)) {
-    console.log(
+    logger.verbose(
       `${option.name} not defined. Using default value: ${option.default}`
     );
     options[option.name] = option.default;
+  } else {
+    logger.verbose(
+      `${option.name} defined. Using value: ${options[option.name]}`
+    );
   }
 });
-
-console.log(options);
 
 // Socket setup
 const io = new Server(
@@ -28,7 +64,7 @@ const io = new Server(
 );
 
 io.on("connection", (socket: Socket) => {
-  console.log("Made socket connection");
+  logger.verbose("Made socket connection");
 
   socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
 
@@ -38,4 +74,4 @@ io.on("connection", (socket: Socket) => {
   });
 });
 
-console.log("Server started");
+logger.info("Server started");
