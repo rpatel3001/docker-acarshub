39960039fd2e9189a56b312ec75459f40e80ad7c
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-10-15 09:48:16 -0600

    print ACARS stats every five minutes

--- acarshub/acars-server/src/message-receiver.ts
+++ acarshub/acars-server/src/message-receiver.ts
@@ -8,7 +8,11 @@ export class MessageReceiver {
   private _logger: Logger;
   private _sock = new zmq.Subscriber();
   private _acars_converter: convertACARS;
-  private _handler;
+  private _handler: AircraftHandler;
+
+  private _total_messages: number = 0;
+  private _total_messages_since_last_update: number = 0;
+  private _interval;
 
   constructor(
     message_type: string,
@@ -22,6 +26,8 @@ export class MessageReceiver {
     this._logger = logger;
     this._handler = handler;
     this._acars_converter = acars_converter;
+
+    this._interval = setInterval(() => this.print_stats(), 5 * 60000);
   }
 
   watch_for_messages = async (): Promise<void> => {
@@ -37,8 +43,11 @@ export class MessageReceiver {
         const decoded_message = this._acars_converter.decode_acars_message(
           JSON.parse(msg.toString())
         );
-        if (decoded_message)
+
+        if (decoded_message) {
+          this.increment_totals();
           this._handler.process_acars_message(decoded_message);
+        }
       } catch (e) {
         this._logger.error(`Error processing message: ${e}`);
       }
@@ -52,4 +61,28 @@ export class MessageReceiver {
     this._sock.disconnect(`tcp://${this._source_url}`);
     this._logger.info(`ZMQ Connection to ${this._source_url} closed`);
   };
+
+  increment_totals(): void {
+    this._total_messages += 1;
+    this._total_messages_since_last_update += 1;
+  }
+
+  print_stats(): void {
+    if (!this._logger) {
+      console.log("PANIC!", this);
+      return;
+    }
+    this._logger.info(
+      `Total ${this._message_type.toUpperCase()} messages for ${
+        this._source_url
+      } in the last five minutes: ${this._total_messages_since_last_update}`
+    );
+    this._logger.info(
+      `Total ${this._message_type.toUpperCase()} messages for ${
+        this._source_url
+      }: ${this._total_messages}`
+    );
+
+    this._total_messages_since_last_update = 0;
+  }
 }
